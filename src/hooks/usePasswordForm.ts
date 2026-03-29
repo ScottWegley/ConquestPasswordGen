e438c03328ce23c5e0538b1d445a99c80a837b6c
Scott Wegley <scbrwegley@protonmail.ch>
2026-03-29 09:57:01 -0700

    getters and setters for password

--- src/hooks/usePasswordForm.ts
+++ src/hooks/usePasswordForm.ts
@@ -10,7 +10,6 @@ export function usePasswordForm() {
   const [pokemon, setPokemon] = useState(POKEMON_LIST[0])
 
   const encode = () => {
-    // TODO: implement encoding logic
   }
 
   const decode = () => {
@@ -39,7 +38,54 @@ export class Password {
     this._region = region;
     this._category = category;
     this._flag = (flag >= 0 && flag <= 63 ? flag : 0);
-    this._pokemon = (POKEMON_LIST.indexOf(pokemon) != -1 ? pokemon : "Eevee")
-    this._scramblePattern = (scramblePattern == -1 ? scramblePattern : Math.random() * 31)
+    this._pokemon = (POKEMON_LIST.indexOf(pokemon) != -1 ? pokemon : "Eevee");
+    this._scramblePattern = (scramblePattern == -1 ? scramblePattern : Math.random() * 31);
   }
+
+  public get region() : Region {
+    return this._region;
+  }
+
+  public get category() : Category {
+    return this._category;
+  }
+  
+  public get flag() : number {
+    return this._flag;
+  }
+  
+  public get pokemon() : string {
+    return this._pokemon;
+  }
+  
+  public get scramblePattern() : number {
+    return this._scramblePattern;
+  }
+
+  public set region(v : Region) {
+    this._region = v;
+  }
+
+  public set category(v : Category) {
+    this._category = v;
+  }
+  
+  public set flag(v : number) {
+    if(v > (this._category === "event" ? 4 : 63) || v < 0) {
+      throw new Error(`${this._category} passwords are constrained to 0-${this._category === "event" ? 4 : 63}`)
+    }
+    this._flag = v;
+  }
+
+  public set pokemon(v : string) {
+    if(this._category === "event"){
+      throw new Error("Pokemon data is not used in event passwords.");
+    }
+    this._pokemon = v;
+  }
+  
+  /** The scramble pattern must be a number between 0 and 31.  If set to a value outside this range, it will instead be set to value modulo 31. */
+  public set scramblePattern(v : number) {
+    this._scramblePattern = (v > 31 || v < 0 ? v % 31 : v);
+  }  
 }
