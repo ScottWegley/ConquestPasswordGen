ed344e0168eaef2dca98886267add78ad1674665
Scott Wegley <scbrwegley@protonmail.ch>
2026-03-29 21:52:28 -0700

    implemented encoding, need to do primary checksum and character conversion

--- src/hooks/usePasswordForm.ts
+++ src/hooks/usePasswordForm.ts
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { POKEMON_LIST } from '../lib/data'
+import { JP_SCRAMBLE_CODES, NA_SCRAMBLE_CODES, POKEMON_LIST } from '../lib/data'
 import type { Region, Category } from '../lib/data'
 
 export function usePasswordForm() {
@@ -35,6 +35,7 @@ export class Password {
   private _pokemon: string;
   private _scramblePattern: number;
 
+  /** Creates a new Password structure. If scramblePattern is not provided, or is specified as -1, a random value between 0 and 31 inclusive will be used. */
   constructor(region: Region = 'na', category: Category = 'pokemon', flag: number = 0, pokemon: string = "Eevee", scramblePattern: number = -1) {
     this._region = region;
     this._category = category;
@@ -94,4 +95,64 @@ export class Password {
     this._scramblePattern = (v > 31 || v < 0 ? v % 31 : v);
   }
   // #endregion
+
+  public encode(): string {
+    // First we construct the raw bytes containing the password data
+    let rawBytes: number[] = [];
+    rawBytes[0] = (this._category === 'pokemon' ? 0 : 1);
+    rawBytes[1] = this._flag;
+    rawBytes[2] = this._scramblePattern;
+    rawBytes[3] = rawBytes[2] + 1;
+    rawBytes[4] = rawBytes[3] + 2;
+    rawBytes[5] = POKEMON_LIST.indexOf(this._pokemon);
+
+    console.log(`Raw bytes: ${rawBytes.map(b => b.toString(16).padStart(2, '0')).join(' ')}`);
+
+    // Second we calculate the secondary checksum of the raw bytes
+    let checksum = rawBytes[5];
+    for (let i = 0; i < 5; i++) {
+      checksum += rawBytes[i] & 0x3f;
+    }
+    console.log(`Sum of raw bytes: ${checksum.toString(16).padStart(2, '0')}`);
+    checksum = checksum & 0x3ff;
+
+    console.log(`Checksum: ${checksum.toString(16).padStart(2, '0')}`);
+
+    // Third we embed the checksum back into the raw bytes
+    for(let i = 0; i < 6; i++) {
+      rawBytes[i] |= ((checksum >> (i * 2)) & 3) << 6;
+    }
+    console.log(`Raw bytes with checksum: ${rawBytes.map(b => b.toString(16).padStart(2, '0')).join(' ')}`);
+
+    // Fourth we apply the scramble pattern
+    let scrambleCode = (this._region === 'na' ? NA_SCRAMBLE_CODES : JP_SCRAMBLE_CODES)[this._scramblePattern].toString(16).padStart(6, '0').split('').map(Number);
+    console.log(`Scramble code: ${scrambleCode.map(n => n.toString(16)).join('')}`);
+
+    let shuffledBytes: number[] = [];
+    for (let i = 0; i < 6; i++) {
+      shuffledBytes[scrambleCode[i]] = rawBytes[i] ^ i;
+    }
+    console.log(`Shuffled bytes: ${shuffledBytes.map(b => b.toString(16).padStart(2, '0')).join(' ')}`);
+
+    // Fifth we convert the shuffled bytes into the final password string
+    let passBytes: number[] = [];
+    passBytes[0] = shuffledBytes[0] % 0x39;
+    passBytes[3] = shuffledBytes[0] / 0x39;
+
+    passBytes[1] = shuffledBytes[1] % 0x39;
+    passBytes[3] |= (shuffledBytes[1] / 0x39) << 3;
+
+    passBytes[2] = shuffledBytes[2] % 0x39;
+    passBytes[4] = shuffledBytes[2] / 0x39;
+
+    passBytes[5] = shuffledBytes[3] % 0x39;
+    passBytes[8] = shuffledBytes[3] / 0x39;
+
+    passBytes[6] = shuffledBytes[4] % 0x39;
+    passBytes[8] |= (shuffledBytes[4] / 0x39) << 3;
+
+    passBytes[7] = shuffledBytes[5] % 0x39;
+    passBytes[9] = shuffledBytes[5] / 0x39;
+    return '';
+  }
 }
