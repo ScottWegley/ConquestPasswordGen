78d4e715f079b0ee33a9690843ef1143aea1f271
Scott Wegley <scbrwegley@protonmail.ch>
2026-03-29 21:10:41 -0700

    regions for structure, debug lines for testing the password class

--- src/hooks/usePasswordForm.ts
+++ src/hooks/usePasswordForm.ts
@@ -10,6 +10,7 @@ export function usePasswordForm() {
   const [pokemon, setPokemon] = useState(POKEMON_LIST[0])
 
   const encode = () => {
+    let passwd: Password = new Password(region, category, flag, pokemon);
   }
 
   const decode = () => {
@@ -39,53 +40,58 @@ export class Password {
     this._category = category;
     this._flag = (flag >= 0 && flag <= 63 ? flag : 0);
     this._pokemon = (POKEMON_LIST.indexOf(pokemon) != -1 ? pokemon : "Eevee");
-    this._scramblePattern = (scramblePattern == -1 ? scramblePattern : Math.random() * 31);
+    this._scramblePattern = (scramblePattern != -1 && scramblePattern <= 31 && scramblePattern >= 0 ? scramblePattern : Math.round(Math.random() * 31));
+    console.log(`Password structure created with region: ${this._region}, category: ${this._category}, flag: ${this._flag}, pokemon: ${this._pokemon}, scramblePattern: ${this._scramblePattern}`);
   }
 
-  public get region() : Region {
+  // #region Getters
+  public get region(): Region {
     return this._region;
   }
 
-  public get category() : Category {
+  public get category(): Category {
     return this._category;
   }
-  
-  public get flag() : number {
+
+  public get flag(): number {
     return this._flag;
   }
-  
-  public get pokemon() : string {
+
+  public get pokemon(): string {
     return this._pokemon;
   }
-  
-  public get scramblePattern() : number {
+
+  public get scramblePattern(): number {
     return this._scramblePattern;
   }
+  // #endregion
 
-  public set region(v : Region) {
+  // #region Setters
+  public set region(v: Region) {
     this._region = v;
   }
 
-  public set category(v : Category) {
+  public set category(v: Category) {
     this._category = v;
   }
-  
-  public set flag(v : number) {
-    if(v > (this._category === "event" ? 4 : 63) || v < 0) {
+
+  public set flag(v: number) {
+    if (v > (this._category === "event" ? 4 : 63) || v < 0) {
       throw new Error(`${this._category} passwords are constrained to 0-${this._category === "event" ? 4 : 63}`)
     }
     this._flag = v;
   }
 
-  public set pokemon(v : string) {
-    if(this._category === "event"){
+  public set pokemon(v: string) {
+    if (this._category === "event") {
       throw new Error("Pokemon data is not used in event passwords.");
     }
     this._pokemon = v;
   }
-  
+
   /** The scramble pattern must be a number between 0 and 31.  If set to a value outside this range, it will instead be set to value modulo 31. */
-  public set scramblePattern(v : number) {
+  public set scramblePattern(v: number) {
     this._scramblePattern = (v > 31 || v < 0 ? v % 31 : v);
-  }  
+  }
+  // #endregion
 }
