1743c00fa1e95eb1b151704e826373d5ce5a9c85
Scott Wegley <scbrwegley@pm.me>
2026-04-01 16:31:30 -0700

    setup development on nix, switch to bun

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "ConquestPasswordGen dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forAllSystems ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          bun
+        ];
+      };
+    });
+  };
+}
